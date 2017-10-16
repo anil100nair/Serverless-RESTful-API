2254e3e68149333d8fd15a45e433125c150a4949
anilnair <anil100nair@gmail.com>
2017-10-16 07:08:51 +0530

    Added create handler

--- user/create.js
+++ user/create.js
@@ -1,58 +1,48 @@
 'use strict';
 
-const db = require('../lib/index');
-const mongoose = require('mongoose');
+const dbAccess = require('../lib/index');
 const usersModel = require('../model/users');
+const dbInstance = new dbAccess();
 
-module.exports.hello = (event, context, callback) => {
-
-  console.log(mongoose.connection.readyState);
-  if (mongoose.connection.readyState !== 1) {
-    const mongoUrl = process.env.MONGOLAB_URL_TODO;
-    
-    db = mongoose.connect(mongoUrl, {
-        useMongoClient: true
-    })
-    .then(() => {
-        console.log("Connection to Database Established.");
-    })
-    .catch(err =>  {
-        console.log("Error while connecting to Database", err)
-    });
-  }
-
-  if ( !event.body.name || !event.body.phone ) {
-    const reeponse = {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "Invalid Data",
-        input: event,
-      }),
-    };
-    callback(null, response);
-  }
-  new usersModel({
-    name: event.body.name,
-    phone: event.body.phone
-  }).save((err) => {
-    if (err) {
-      console.log(err);
-      const reeponse = {
-        statusCode: 500,
+module.exports.createUser = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  
+  dbInstance.connectDB().then(() => {
+    event.body = JSON.parse(event.body);
+    if ( !event.body.name || !event.body.phone ) {
+      const response = {
+        statusCode: 400,
         body: JSON.stringify({
-          message: err.message,  // You need to write the response object and call callback
+          message: "Invalid Data",
           input: event,
         }),
       };
       callback(null, response);
     }
-    const response = {
-      statusCode: 201,
-      body: JSON.stringify({
-        message: "User successfully created.",
-        input: event,
-      }),
-    };
-    callback(null, response);
-  });
+    console.log("Creating Model with given data.");
+    new usersModel({
+      name: event.body.name,
+      phone: event.body.phone
+    }).save((err) => {
+      if (err) {
+        console.log(err);
+        const reeponse = {
+          statusCode: 500,
+          body: JSON.stringify({
+            message: err.message,
+            input: event,
+          }),
+        };
+        callback(null, response);
+      }
+      const response = {
+        statusCode: 201,
+        body: JSON.stringify({
+          message: "User successfully created.",
+          input: event,
+        }),
+      };
+      callback(null, response);
+    });
+  });  
 };
